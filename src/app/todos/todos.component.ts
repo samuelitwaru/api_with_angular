6c4dd31e108bb0e4e41b481e8a8b7494a5609848
samuelitwaru <samuelitwaru@gmail.com>
2019-08-04 17:40:50 +0300

    Third Commit

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../todos.service';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+	
+	todos: any = [];
+	todo: any;
+	action: any;
+	name: any;
+	isDone: boolean;
 
-  constructor() { }
+  constructor(private todoService: TodosService) { }
 
   ngOnInit() {
+  	this.todoService.getTodos().subscribe(todos => {
+  		this.todos = todos;
+  	})
+  }
+
+  addTodos(event){
+  	event.preventDefault();
+  	let newTodo = {
+  		name: this.name,
+  		action: this.action,
+  		isDone: false
+  	};
+
+  	this.todoService.addTodos(newTodo).subscribe(todo => {
+  		this.todo.push(todo);
+  		this.name = '';
+  		this.action = '';
+  	});
+  }
+
+  deleteTodo(id) {
+  	let todos = this.todos;
+  	this.todoService.deleteTodo(id).subscribe(data => {
+  		const index = this.todos.findIndex(todo=>{todo._id == id;})
+  		todos.splice(index, 1)
+  	});
+  }
+
+  updateStatus(todo) {
+  	var _todo = {
+  		_id: todo._id,
+  		action: todo.action,
+  		isDone: !todo.isDone
+  	}
+
+  	this.todoService.updateStatus(_todo).subscribe(data=>{
+  		const index = this.todos.findIndex(todo => {todo._id == _todo._id})
+  		this.todos[index] = _todo;
+  	})
+  }
+
+  choice(todo){
+  	console.log(todo);
+  	return todo.isDone;
   }
 
 }
